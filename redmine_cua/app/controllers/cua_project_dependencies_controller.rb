391e4616e43b4e227cdd9e70a98c05172b15a493
macintoshplus <jean-baptiste@nahan.fr>
2016-06-10 17:44:59 +0200

    add link to project, add sort table

--- redmine_cua/app/controllers/cua_project_dependencies_controller.rb
+++ redmine_cua/app/controllers/cua_project_dependencies_controller.rb
@@ -4,13 +4,39 @@ class CuaProjectDependenciesController < ApplicationController
   before_filter :find_project_by_project_id, :authorize, :only => [:index]
 
   def index
-  	@dependencies = CuaDependencies.by_project(@project)
+
+    @field = params[:field]
+
+    @order = params[:order]
+
+    if @field.nil?
+      @field = "project_id"
+    end
+
+    if @order.nil?
+      @order = "ASC"
+    end
+
+  	@dependencies = CuaDependencies.by_project(@project).order(@field+" "+@order)
 
   	@states = CuaDependencies.by_project(@project).group(:state).count
   end
 
   def indexall
-  	@dependencies = CuaDependencies.all
+
+    @field = params[:field]
+
+    @order = params[:order]
+
+    if @field.nil?
+      @field = "project_id"
+    end
+
+    if @order.nil?
+      @order = "ASC"
+    end
+    
+  	@dependencies = CuaDependencies.all.order(@field+" "+@order)
 
   	@states = CuaDependencies.group(:state).count
   end
